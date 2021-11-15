4d4c8d6f596f869770d107f0ad447be09b4d3599
Lucy134 <67610043+Lucy134@users.noreply.github.com>
2021-11-15 17:37:48 -0600

    lol br tags to the rescue

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/RegistrationDTO.java
@@ -0,0 +1,13 @@
+package org.launchcode.javawebdevtechjobsauthentication.models;
+
+public class RegistrationDTO extends LoginDTO{
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+}
